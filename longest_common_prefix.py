d9e7aafd497f111c08d1402b4525b163eef1ae32
zainabbaker290 <119340513@umail.ucc.ie>
2022-10-06 13:06:33 +0100

    list pos stars at 0 why?

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,33 +1,32 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
         common_string = ""
-        base_word = 0
-        #to make sure index is never out of range 
-        if len(strs[base_word]) < len(strs[base_word + 1]):
-            length = len(strs[base_word])
-        else:
-           length = len(strs[base_word + 1]) 
+        base_word = strs[0]
+        i = 0
+        list_pos = 1
 
-        j = len(strs) -1 
-        while j > 0:
-            for i in range(length): 
-                if strs[base_word][i] == strs[base_word + 1][i]: #comparing the base word to the word after
-                    common_string += strs[base_word][i]
-            if common_string != "" and j != 1:
-                common_string = "" #resetting common word 
-                base_word += 1 
-                j -= 1 
-            elif common_string != "":
-                return common_string
+        if len(strs) == 1:
+            return base_word
+        
+        #list pos is 0????? 
+        for list_pos in range(len(strs)):
+            if len(base_word) < len(strs[list_pos]):
+                length = len(base_word)
             else:
-                return "none"
-        return common_string
+                length = len(strs[list_pos]) 
+            for i in range(length):
+                if base_word[i] == strs[list_pos][i]:
+                    common_string += base_word[i]
+                else:
+                    break
+            
+            if strs[list_pos] != strs[-1]:
+                base_word = common_string
+                common_string = ""
+            else:
+                return common_string
 
-#issue
-#i know how to fix the issue in my head but not with code
-#i need to reset the base word to be the common word
-#but im not too sure how to do this
-
+        return base_word
 
 S = Solution()
-print(S.longestCommonPrefix(["flower","flowx","fligxht"]))
+print(S.longestCommonPrefix(["aaa","aa", "aaa"]))
