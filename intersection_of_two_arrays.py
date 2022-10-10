8ce373cb644106a1982b0027356a87dad8340559
zainabbaker290 <119340513@umail.ucc.ie>
2022-10-10 15:42:46 +0100

    coming back to it

--- intersection_of_two_arrays.py
+++ intersection_of_two_arrays.py
@@ -1,15 +1,16 @@
 class Solution:
     def intersect(self, nums1, nums2):
-        l1 = len(nums1)
-        l2 = len(nums2)
+        set1 = set(nums1)
+        set2 = set(nums2)
 
-        for num in nums1:
-            for n in nums2:
-                if n == num:
-                    print(n, num)
+        return list(set1 & set2)
+        
+
+                
 
             
-        return
+
+        
 
 S = Solution()
 print(S.intersect([1,2,2,1], [2,2]))
